bd64e66060316f41a30fe190eb846b4142696ca8
Dheeraj Gupta <djgupta820@gmail.com>
2023-05-01 19:14:40 +0530

    all functionalities done

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let openMenu = document.getElementById('openMenu')
 let closeMenu = document.getElementById('closeMenu')
 let mainMenu = document.getElementById('mainMenu')
+let form = document.getElementById('form')
 
 openMenu.addEventListener('click', ()=>{
     mainMenu.style.display = 'flex';
@@ -9,4 +10,19 @@ openMenu.addEventListener('click', ()=>{
 
 closeMenu.addEventListener('click', ()=>{
     mainMenu.style.top = '-110%'
+})
+
+form.addEventListener('submit', (e)=>{
+    e.preventDefault()
+    let btn = document.getElementById('form-submit')
+    btn.disabled = true
+    fetch(form.action, {
+        method:"POST",
+        body: new FormData(document.getElementById('form')),
+    }).then(
+        response => response.json()
+    ).then((html) =>{
+        alert('Your query has been submitted.')
+        btn.disabled = false
+    })
 })
