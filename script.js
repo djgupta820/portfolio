f2c9ad78a39b1ca092f07c762206f1137511c8de
Dheeraj Gupta <djgupta820@gmail.com>
2023-05-01 19:51:35 +0530

    navigation bugs fixed

--- script.js
+++ script.js
@@ -3,6 +3,33 @@ let closeMenu = document.getElementById('closeMenu')
 let mainMenu = document.getElementById('mainMenu')
 let form = document.getElementById('form')
 
+let home = document.querySelector('.fa-house').parentElement
+let about = document.querySelector('.fa-user').parentElement
+let skills = document.querySelector('.fa-hashtag').parentElement
+let projects = document.querySelector('.fa-diagram-project').parentElement
+let contact = document.querySelector('.fa-address-card').parentElement
+
+home.addEventListener('click', ()=>{
+    mainMenu.style.top = '-110%'
+})
+
+about.addEventListener('click', ()=>{
+    mainMenu.style.top = '-110%'
+})
+
+
+skills.addEventListener('click', ()=>{
+    mainMenu.style.top = '-110%'
+})
+
+projects.addEventListener('click', ()=>{
+    mainMenu.style.top = '-110%'
+})
+
+contact.addEventListener('click', ()=>{
+    mainMenu.style.top = '-110%'
+})
+
 openMenu.addEventListener('click', ()=>{
     mainMenu.style.display = 'flex';
     mainMenu.style.top = '0';
@@ -25,4 +52,5 @@ form.addEventListener('submit', (e)=>{
         alert('Your query has been submitted.')
         btn.disabled = false
     })
+    form.reset()
 })
